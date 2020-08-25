22ed44cb59aef10071d1c86d59c06b634262102a
Matt Slater <matthewslater1998@gmail.com>
2020-08-25 11:49:09 +0100

    Font updates

--- resources/views/templates/styleguide/foundation/fonts.blade.php
+++ resources/views/templates/styleguide/foundation/fonts.blade.php
@@ -1,4 +1,15 @@
-@foreach ($weights as $weight)
-	<p style="font-weight: {{ $weight }}">The quick brown fox jumps over the lazy dog</p>
-	<p style="font-weight: {{ $weight }}; font-style: italic">The quick brown fox jumps over the lazy dog</p>
-@endforeach
+	<h1 class="e-h1">The quick brown fox jumps over the lazy dog</h1>
+
+	<h2 class="e-h2">The quick brown fox jumps over the lazy dog</h2>
+
+	<h3 class="e-h3">The quick brown fox jumps over the lazy dog</h3>
+
+	<h4 class="e-h4">The quick brown fox jumps over the lazy dog</h4>
+
+	<h5 class="e-h5">The quick brown fox jumps over the lazy dog</h5>
+
+	<h6 class="e-h6">The quick brown fox jumps over the lazy dog</h6>
+
+	<p>The quick brown fox jumps over the lazy dog</p>
+
+	<p style="font-style: italic">The quick brown fox jumps over the lazy dog</p>
