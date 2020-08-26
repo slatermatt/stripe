c6e224bd2c5a3153242997a8445c0974bf9c41bc
Matt Slater <matthewslater1998@gmail.com>
2020-08-26 16:43:28 +0100

    WIP why stripe block styles

--- resources/views/partials/blocks/why-stripe.blade.php
+++ resources/views/partials/blocks/why-stripe.blade.php
@@ -1,15 +1,31 @@
-<section>
-	<p>{!! $subTitle !!}</p>
+<section class="relative overflow-hidden">
+	@include('partials/guides')
+
+	<container>
+		<div class="grid row-gap-16">
+			<div class="grid gap-6">
+				<p class="e-h3 text-brand-purple px-4">{!! $subTitle !!}</p>
+				
+				<h1 class="e-h2 px-4">{!! $title !!}</h1>
+			</div>
 	
-	<h1>{!! $title !!}</h1>
+			@foreach ($features as $feature)
+				<div class="grid row-gap-2">
+					<icon
+						name="{!! $feature['icon'] !!}"
+						size="w-6 h-6 lg:w-10 lg:h-10"
+						class="pl-4 text-brand-purple"
+					></icon>
 	
-	@foreach ($features as $feature)
-		<div>
-			<icon name="{!! $feature['icon'] !!}" ></icon>
+					<h2 class="relative e-h5 px-4">
+						<span class="absolute h-full w-px left-0 bg-brand-purple"></span>
 	
-			<h2>{!! $feature['title'] !!}</h2>
-	
-			<p>{!! $feature['content'] !!}</p>
+						<span class="text-brand-blue-dark">{!! $feature['title'] !!}</span>
+					</h2>
+			
+					<p class="e-body--small px-4">{!! $feature['content'] !!}</p>
+				</div>
+			@endforeach
 		</div>
-	@endforeach
+	</container>
 </section>
