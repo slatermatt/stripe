8ff58955eb570b23d12756a552b904ac66ed94b7
Matt Slater <matthewslater1998@gmail.com>
2020-08-26 16:15:21 +0100

    Updates guides colours

--- resources/views/partials/guides.blade.php
+++ resources/views/partials/guides.blade.php
@@ -1,12 +1,12 @@
 <div class="absolute w-full h-full overflow-visible">
-	<div class="relative w-full h-full top-0 left-0 bg-white overflow-hidden">
+	<div class="relative w-full h-full top-0 left-0 overflow-hidden {!! $bg ?? '' !!}">
 		<div class="absolute w-full h-full top-0 left-0 px-4 pointer-events-none xl:max-w-screen">
 			<div class="grid md:grid-cols-2 relative max-w-container h-full mx-auto lg:grid-cols-4">
-				<span class="w-px border-l border-dashed border-grey-400"></span>
-				<span class="hidden xs:block w-px border-l border-dashed border-grey-400"></span>
-				<span class="w-px border-l border-dashed border-grey-400"></span>
-				<span class="hidden xs:block w-px border-l border-dashed border-grey-400"></span>
-				<span class="w-px border-l border-dashed border-grey-400 absolute top-0 right-0 h-full"></span>
+				<span class="w-px border-l border-dashed border-off-white opacity-25"></span>
+				<span class="hidden xs:block w-px border-l border-dashed border-off-white opacity-25"></span>
+				<span class="w-px border-l border-dashed border-off-white opacity-25"></span>
+				<span class="hidden xs:block w-px border-l border-dashed border-off-white opacity-25"></span>
+				<span class="w-px border-l border-dashed border-off-white opacity-25 absolute top-0 right-0 h-full"></span>
 			</div>
 		</div>
 	</div>
