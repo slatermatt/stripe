5dc99c93e0581d25f4c6aeef6f6acc88da0457f4
Matt Slater <matthewslater1998@gmail.com>
2020-08-27 09:48:47 +0100

    Completes styles for why stripe block

--- resources/views/partials/blocks/why-stripe.blade.php
+++ resources/views/partials/blocks/why-stripe.blade.php
@@ -2,30 +2,34 @@
 	@include('partials/guides')
 
 	<container>
-		<div class="grid row-gap-16">
-			<div class="grid gap-6">
-				<p class="e-h3 text-brand-purple px-4">{!! $subTitle !!}</p>
-				
-				<h1 class="e-h2 px-4">{!! $title !!}</h1>
-			</div>
-	
-			@foreach ($features as $feature)
-				<div class="grid row-gap-2">
-					<icon
-						name="{!! $feature['icon'] !!}"
-						size="w-6 h-6 lg:w-10 lg:h-10"
-						class="pl-4 text-brand-purple"
-					></icon>
-	
-					<h2 class="relative e-h5 px-4">
-						<span class="absolute h-full w-px left-0 bg-brand-purple"></span>
-	
-						<span class="text-brand-blue-dark">{!! $feature['title'] !!}</span>
-					</h2>
-			
-					<p class="e-body--small px-4">{!! $feature['content'] !!}</p>
+		<div class="grid row-gap-8 md:row-gap-12 xl:row-gap-16">
+			<div class="grid sm:grid-cols-4">
+				<div class="grid gap-6 sm:col-span-3">
+					<p class="e-h3 text-brand-purple px-4">{!! $subTitle !!}</p>
+					
+					<h1 class="e-h2 px-4">{!! $title !!}</h1>
 				</div>
-			@endforeach
+			</div>
+
+			<div class="grid items-start md:grid-cols-2 xl:grid-cols-4">
+				@foreach ($features as $feature)
+					<div class="grid row-gap-2">
+						<icon
+							name="{!! $feature['icon'] !!}"
+							size="w-6 h-6 lg:w-10 lg:h-10"
+							class="pl-4 text-brand-purple"
+						></icon>
+		
+						<h2 class="relative e-h5 px-4">
+							<span class="absolute h-full w-px left-0 bg-brand-purple"></span>
+		
+							<span class="text-brand-blue-dark">{!! $feature['title'] !!}</span>
+						</h2>
+				
+						<p class="e-body--small px-4">{!! $feature['content'] !!}</p>
+					</div>
+				@endforeach
+			</div>
 		</div>
 	</container>
 </section>
