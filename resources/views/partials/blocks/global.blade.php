7d8f6ee949e68e7f1b1f774518166a787c8e4c2f
Matt Slater <matthewslater1998@gmail.com>
2020-08-27 10:13:56 +0100

    Adds styles for global block

--- resources/views/partials/blocks/global.blade.php
+++ resources/views/partials/blocks/global.blade.php
@@ -1,15 +1,31 @@
-<section>
-	<p>{!! $subTitle !!}</p>
-	
-	<h1>{!! $title !!}</h1>
+<section class="relative overflow-hidden">
+	@include('partials/guides', ['bg' => 'bg-brand-blue-dark'])
 
-	<p>{!! $content !!}</p>
-	
-	@foreach ($stats as $stat)
-		<div>
-			<h2>{!! $stat['figure'] !!}</h2>
-	
-			<p>{!! $stat['description'] !!}</p>
+	<container>
+		<div class="grid row-gap-8 md:row-gap-12 xl:row-gap-16">
+			<div class="grid sm:grid-cols-2">
+				<div class="grid gap-6 px-4 sm:pr-24 sm:col-span-1">
+					<p class="e-h4 text-brand-blue-light">{!! $subTitle !!}</p>
+					
+					<h1 class="e-h2 text-white">{!! $title !!}</h1>
+
+					<p class="text-off-white">{!! $content !!}</p>
+				</div>
+			</div>
+
+			<div class="grid items-start md:grid-cols-2 xl:grid-cols-4">
+				@foreach ($stats as $stat)
+					<div class="grid row-gap-2">
+						<h2 class="relative e-h3 px-4">
+							<span class="absolute h-full w-px left-0 bg-brand-blue-light"></span>
+		
+							<span class="text-white">{!! $stat['figure'] !!}</span>
+						</h2>
+				
+						<p class="e-body--small px-4 text-off-white">{!! $stat['description'] !!}</p>
+					</div>
+				@endforeach
+			</div>
 		</div>
-	@endforeach
+	</container>
 </section>
