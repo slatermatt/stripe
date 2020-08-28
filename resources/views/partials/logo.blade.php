09c883dfbaca300c2fcffa534a3fd419e28f49ca
mattslater <matthew.slater@engageinteractive.co.uk>
2020-08-28 12:04:17 +0100

    Adds stripe logo

---
+++ resources/views/partials/logo.blade.php
@@ -0,0 +1,12 @@
+<div class="max-w-logo-xl w-full">
+	<placeholder class="pt-logo">
+		<h1>
+			<a href="{{ $links['home'] }}">
+				<icon
+					name="logo"
+					size="w-full h-full"
+				></icon>
+			</a>
+		</h1>
+	</placeholder>
+</div>
