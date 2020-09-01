0faf823f09bd1a18e6cc9e27dbfd6784d6e735ad
Matt Slater <matthewslater1998@gmail.com>
2020-09-01 11:10:43 +0100

    Adds page default data for desktop nav

--- app/Http/ViewComposers/PageDefaultsViewComposer.php
+++ app/Http/ViewComposers/PageDefaultsViewComposer.php
@@ -149,6 +149,215 @@ class PageDefaultsViewComposer extends BaseViewComposer
                         'url' => route('templates.show', 'sign-in/index'),
                     ],
                 ],
+                'desktop' => [
+                    'products' => [
+                        'title' => 'Products',
+                        'categories' => [
+                            [
+                                'title' => 'Payments',
+                                'items' => [
+                                    [
+                                        'icon' => 'chevron-up',
+                                        'title' => 'Payments',
+                                        'description' => 'Online Payments',
+                                        'url' => route('templates.show', 'payments/view'),
+                                    ],
+                                    [
+                                        'icon' => 'chevron-right',
+                                        'title' => 'Terminal',
+                                        'description' => 'In-person payments',
+                                        'url' => route('templates.show', 'payments/view'),
+                                    ],
+                                    [
+                                        'icon' => 'chevron-down',
+                                        'title' => 'Connect',
+                                        'description' => 'Payments for platforms',
+                                        'url' => route('templates.show', 'payments/view'),
+                                    ],
+                                    [
+                                        'icon' => 'chevron-left',
+                                        'title' => 'Billing',
+                                        'description' => 'Subscriptions & invoicing',
+                                        'url' => route('templates.show', 'payments/view'),
+                                    ],
+                                ],
+                            ],
+                            [
+                                'title' => 'Payouts',
+                                'items' => [
+                                    [
+                                        'icon' => 'chevron-up',
+                                        'title' => 'Payouts',
+                                        'description' => 'Programmatic payouts',
+                                        'url' => route('templates.show', 'payouts/view'),
+                                    ],
+                                    [
+                                        'icon' => 'chevron-right',
+                                        'title' => 'Issuing',
+                                        'description' => 'Card creation',
+                                        'url' => route('templates.show', 'payouts/view'),
+                                    ],
+                                ],
+                            ],
+                            [
+                                'title' => 'Business operations',
+                                'items' => [
+                                    [
+                                        'icon' => 'chevron-up',
+                                        'title' => 'Radar',
+                                        'description' => 'Fraud & risk management',
+                                        'url' => route('templates.show', 'operations/view'),
+                                    ],
+                                    [
+                                        'icon' => 'chevron-right',
+                                        'title' => 'Sigma',
+                                        'description' => 'Custom reports',
+                                        'url' => route('templates.show', 'operations/view'),
+                                    ],
+                                    [
+                                        'icon' => 'chevron-down',
+                                        'title' => 'Atlas',
+                                        'description' => 'Startup incorporation',
+                                        'url' => route('templates.show', 'operations/view'),
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                    'developers' => [
+                        'title' => 'Developers',
+                        'docs' => [
+                            'icon' => 'chevron-right',
+                            'title' => 'Documentation',
+                            'description' => 'Start integrating Stripes products and tools',
+                            'url' => route('templates.show', 'docs/index'),
+                        ],
+                        'categories' => [
+                            [
+                                'title' => 'Get started',
+                                'items' => [
+                                    [
+                                        'title' => 'Prebuilt checkout',
+                                        'url' => route('templates.show', 'checkout/index'),
+                                    ],
+                                    [
+                                        'title' => 'Libraries and SDKs',
+                                        'url' => route('templates.show', 'libraries/index'),
+                                    ],
+                                    [
+                                        'title' => 'Plugins',
+                                        'url' => route('templates.show', 'plugins/index'),
+                                    ],
+                                    [
+                                        'title' => 'Code samples',
+                                        'url' => route('templates.show', 'code-samples/index'),
+                                    ],
+                                ],
+                            ],
+                            [
+                                'title' => 'Guides',
+                                'items' => [
+                                    [
+                                        'title' => 'Accept online payments',
+                                        'url' => route('templates.show', 'payments/index'),
+                                    ],
+                                    [
+                                        'title' => 'Manage subscriptions',
+                                        'url' => route('templates.show', 'manage/index'),
+                                    ],
+                                    [
+                                        'title' => 'Send payments',
+                                        'url' => route('templates.show', 'send/index'),
+                                    ],
+                                    [
+                                        'title' => 'Set up in-person payments',
+                                        'url' => route('templates.show', 'in-person/index'),
+                                    ],
+                                ],
+                            ],
+                        ],
+                        'links' => [
+                            [
+                                'icon' => 'chevron-up',
+                                'title' => 'Full API reference',
+                                'url' => route('templates.show', 'api/index'),
+                            ],
+                            [
+                                'icon' => 'chevron-right',
+                                'title' => 'API status',
+                                'url' => route('templates.show', 'api/index'),
+                            ],
+                            [
+                                'icon' => 'chevron-down',
+                                'title' => 'Support',
+                                'url' => route('templates.show', 'api/index'),
+                            ],
+                            [
+                                'icon' => 'chevron-left',
+                                'title' => 'API changelog',
+                                'url' => route('templates.show', 'api/index'),
+                            ],
+                        ],
+                    ],
+                    'company' => [
+                        'links' => [
+                            [
+                                'icon' => 'chevron-up',
+                                'title' => 'About stripe',
+                                'url' => route('templates.show', 'about/index'),
+                            ],
+                            [
+                                'icon' => 'chevron-right',
+                                'title' => 'Jobs',
+                                'url' => route('templates.show', 'careers/index'),
+                            ],
+                            [
+                                'icon' => 'chevron-down',
+                                'title' => 'Customers',
+                                'url' => route('templates.show', 'customers/index'),
+                            ],
+                            [
+                                'icon' => 'chevron-left',
+                                'title' => 'Environment',
+                                'url' => route('templates.show', 'environment/index'),
+                            ],
+                            [
+                                'icon' => 'chevron-up',
+                                'title' => 'Enterprise',
+                                'url' => route('templates.show', 'enterprise/index'),
+                            ],
+                            [
+                                'icon' => 'chevron-right',
+                                'title' => 'Newsroom',
+                                'url' => route('templates.show', 'newsroom/index'),
+                            ],
+                            [
+                                'icon' => 'chevron-down',
+                                'title' => 'Partners',
+                                'url' => route('templates.show', 'partners/index'),
+                            ],
+                        ],
+                        'blog' => [
+                            'icon' => 'chevron-right',
+                            'title' => 'From the blog',
+                            'url' => route('templates.show', 'blog/index'),
+                            'articles' => [
+                                [
+                                    'title' => 'Introducing the Billing customer portal',
+                                    'url' => route('templates.show', 'articles/view'),
+                                ],
+                                [
+                                    'title' => 'Opposing racism',
+                                    'url' => route('templates.show', 'articles/view'),
+                                ],
+                                [
+                                    'title' => 'Stripes remote engineering hub, one year in',
+                                    'url' => route('templates.show', 'articles/view'),
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
             ],
         ];
     }
